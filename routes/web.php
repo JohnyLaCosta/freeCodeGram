9a815fc99c364162cec22c64b6bb2a6893cb6f59
JohnyLaCosta <58307888+kilopico9876@users.noreply.github.com>
2023-09-02 02:30:28 +0100

    Query Builders
    Definição de query builders, alguns exemplos e link para os mesmos

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,14 @@ use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     // return view('welcome');
-    
+
+    /* Buscar todos os users através de um Query Builder */
+    /* $users = DB::table('users')->get();
+    dd($users); */
+
     /*SELECT*/
-    $users = DB::select('select * from users');
-    dd($users);
+    /* $users = DB::select('select * from users');
+    dd($users); - Buscar todos os users.*/
 
     /*INSERT*/
     /* $user = DB::insert('insert into users (name, password, email) values (?,?,?)', [
@@ -37,6 +41,36 @@ Route::get('/', function () {
     /*DELETE*/
     /* $user = DB::delete("delete from users where id = 2");
     dd($user); */
+
+
+    /* USANDO QUERY BUILDER */
+    /* Buscar o user com ID = 1, através de um Query Builder */
+    /* $users = DB::table('users')->where('id', 1)->get();
+    dd($users); */
+
+    /* Insert na DB, através de um Query Builder */
+    /* $user = DB::table('users')->insert([
+        'name' => 'Laura',
+        'email' => 'laura@gmail.com',
+        'password' => 'password'
+    ]); */
+
+    /* Update na DB, através de um Query Builder */
+    /* $user=DB::table('users')->where('id',3)->update(['email'=>'newlaura@gmail.com']); */
+
+    /* Delete na DB, através de um Query Builder */
+    /* $user=DB::table('users')->where('id',3)->delete(); */
+
+    
+    /* FUNÇÕES ÚTEIS PARA AJUDAR A FILTRAR CONTEÚDO NAS QUERYS */
+    /* Função First */
+    /* $users = DB::table('users')->first();
+    dd($users); */
+
+    /* Função Find (serve para encontrar um elemento por id na DB de forma facilitada) */
+    /* $users = DB::table('users')->find(1);
+    dd($users); */
+    /* PS: Existe outras funções interessantes como a chunk, pluck, etc... ver a documentação: https://laravel.com/docs/10.x/collections*/
 });
 
 Route::get('/dashboard', function () {
@@ -49,4 +83,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
